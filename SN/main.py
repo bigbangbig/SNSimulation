bbc01f6df9e32683f066ba620d700f9aa31050bb
longclaw <hosein.latifi@outlook.com>
2017-10-14 11:40:57 +0330

    ایجاد یک گراف با تعداد مشخص از گره ها و یالها

--- SN/main.py
+++ SN/main.py
@@ -3,9 +3,27 @@ import matplotlib.pylab as plt
 import initialize as init
 
 
-g = init.go(50, 150)
+def get_input(node, edge):
+    try:
+        node = int(input("How many nodes? "))
+        edge = int(input("How many edges? "))
+        return True, node, edge
+    except Exception:
+        print("Exception thrown. Did you enter numbers? this only accepts numbers")
+        return False, node, edge
+
+
+nodes = 0
+edges = 0
+res = False
+while not res:
+    res, nodes, edges = get_input(nodes, edges)
+    # if res:
+    #     break
+
+g = init.go(nodes, edges)
 count = g.number_of_edges()
-print("Node count: " + str(count))
+print("Edge count: " + str(count))
 
 options_2 = {
  'with_labels': False,
@@ -18,6 +36,6 @@ options_2 = {
 nx.draw_spring(g, iterations=1, **options_2)
 
 # nx.draw_random(g)
-plt.savefig('Hi')
+plt.savefig('Network')
 plt.show()
 
